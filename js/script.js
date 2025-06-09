5d586e626008b966e1d44e0cb563769279514516
vano604 <vlassivvan@gmail.com>
2025-06-09 14:46:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ function mobileMenu() {
         x.className = "nav";
     }
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxidtVD1x-dEm2m7p_lrF9Qdf1UZwg_8iNTkx3btlK-OYfN4ZfJYtVUIs3PzVIearl1/exec'
+        const form = document.forms['submit-to-google-sheet']
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+        })
